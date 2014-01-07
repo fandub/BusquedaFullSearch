98b80bea48e26cc9d2554400b7ded669d6be59a4
Paula Lineros <pau.lokilla@gmail.com>
2014-01-07 04:23:30 -0300

    cambio
    vista

--- CodeIgniter/application/views/vista.php
+++ CodeIgniter/application/views/vista.php
@@ -6,11 +6,13 @@
 <style type="text/css">
     #buscador input[type=text]{
         padding: 10px; 
-        color: grey;
+        color: black;
         font-weight: bold;
         border-radius: 40px;
         width: 520px; 
         text-align: center;
+        margin: auto;
+        display:block;
     }
 </style>
 
